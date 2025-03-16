8c614b9b8269ac93967147e3564f2d1e5a85f1f8
adel hany <adel58420@gmail.com>
2025-03-16 08:56:41 +0200

    re

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
-import express from 'express';
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRoutes from "../routes/auth.route.js";
+import movieRoutes from "../routes/movie.route.js";
+import tvRoutes from "../routes/tv.route.js";
+import searchRoutes from "../routes/search.route.js";
+import { ENV_VARS } from "../config/envVars.js";
+import { connectDB } from "../config/db.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Express on Vercel'));
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}`));
 
 export default app;
